18b16d3050870bb0eeb8a20a38e530bb52a732d4
yuya <siren.night43@gmail.com>
2019-07-11 01:52:03 +0900

    Fix session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,19 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
+    @user = User.find_by!(email: session_params[:email])
+
+    if @user.authenticate(session_params[:password])
+      sign_in
+      redirect_to root_path
+    else
+      flash.now[:alart] = 'パスワードが正しくありません'
+      render :new
+    end
+  rescue ActiveRecord::RecordNotFound => e
+    flash.now[:alart] = e.message
+    render :new
   end
 
   def destroy
